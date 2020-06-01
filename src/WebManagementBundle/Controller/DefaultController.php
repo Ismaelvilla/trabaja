89381f31082f5b8d8410f21220ad36b7efd95a8f
Urza <ismael.villarreal@hotmail.es>
2020-06-01 17:51:21 +0200

    1Mejora FosUserBundle

--- src/WebManagementBundle/Controller/DefaultController.php
+++ src/WebManagementBundle/Controller/DefaultController.php
@@ -12,7 +12,10 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        //echo "funciona";
+       // die();
         return $this->render('WebManagementBundle:Default:index.html.twig');
+        //return $this->redirectToRoute("fos_user_security_login");
     }
 
 }
