93adbfd95e7d2e20cbfddadd027b94fc0de83c74
Urza <ismael.villarreal@hotmail.es>
2020-06-12 14:11:37 +0200

    Mejora botones Guardar y cancelar

--- web/js/tarea.js
+++ web/js/tarea.js
@@ -12,54 +12,44 @@ $(document).ready(function(){
 
         //se desactiva el boton de NuevaTarea
         $('#nuevaTarea').attr("disabled", true);
-
-      /*  $.ajax({
-           type: 'GET',
-           url: 'nueva-ajax',
-           success: function(respuesta){
-                console.log(respuesta.redirect);
-           }
-        });*/
     })
     //*********************** INICIO ACCIONES CANCELAR*************** //
     $('#Cancelar').click(function(){
         if($('#textNuevaTarea').val()){
-            console.log('tiene texto');
             $('#modalCancelar').modal('show');
         }else{
-            console.log('no tiene texto');
             AccionesCancelar();
         }
     });
 
     $('#btnCancelarModalNo').click(function(){
-        console.log('estamos en no');
         AccionesCancelar();
     });
 
     $('#btnCancelarModalSi').click(function(){
-        console.log('hemos pulsado si');
         $('#Guardar').click();
     });
 
     //Acciones llevadas a cabo cuando se cancela y no se guardan los cambios
     function AccionesCancelar(){
         //se Desactivan los 3 botones de cancelar, guardar, Eliminar
-        DesactivarBotones();
+        estadoBotones(true);
         //desactivamos el texto textNuevaTarea
         $('#textNuevaTarea').attr("disabled", true);
         //ponemos vacio el textNueva Tareas
         $('#textNuevaTarea').val("");
         //activamos el boton NuevaTarea
         $('#nuevaTarea').attr("disabled", false);
+        //este div tiene que estar vacio
+        $('#tareaVacia').html('');
     }
     //************** FIN ACCIONES CANCELAR ******************//
 
     $('#Guardar').click(function(){
-      console.log('llegamos a guardar');
+        $('#tareaVacia').html('');
         if(document.getElementById('textNuevaTarea').value){
             //se Desactivan los 3 botones de cancelar, guardar, Eliminar
-            DesactivarBotones();
+            estadoBotones(true);
             //se activa el boton de NuevaTarea
             $('#nuevaTarea').attr('disabled', false);
             //hacemos el insert en la tabla tareas, mediante ajax
@@ -83,45 +73,35 @@ $(document).ready(function(){
         }
     });
 
-    function DesactivarBotones(){
-      $('#Guardar').attr("disabled", true);
-      $('#Cancelar').attr("disabled", true);
-      $('#Eliminar').attr("disabled", true);
-    }
-
     function estadoBotones(estado){
-      //si es true se desactivan los botones, si es false se activan
-      $('#Guardar').attr("disabled", estado);
-      $('#Cancelar').attr("disabled", estado);
-      $('#Eliminar').attr("disabled", estado);
+        //si es true se desactivan los botones, si es false se activan
+        $('#Guardar').attr("disabled", estado);
+        $('#Cancelar').attr("disabled", estado);
+        $('#Eliminar').attr("disabled", estado);
     }
 
     //cada vez que se haga click en un checkbox comprobamos que
     // si hay alguno seleccionado los botones estaran activados y
     //si no hay ninguno seleccionado los botones estaran desactivados
-    $('.check').click(function(){
-      var activaBotones = false;
-      //recorremos todos los checkbox
-      $(".check").each(function () {
-        //sacamos su atributo id
-        var id = this.id;
-        console.log('prueba '+this.id);
-        if( $('#'+id).prop('checked') ){
-          activaBotones = true;
-          console.log('tiene click');
-        }
+    $('#gridTareas').on('click', '.check', function(e){
+        var activaBotones = false;
+        //recorremos todos los checkbox
+        $(".check").each(function () {
+          //sacamos su atributo id
+          var id = this.id;
+          if( $('#'+id).prop('checked') ){
+            activaBotones = true;
+          }
 
-      });
-      // si activaBotones es false se desactivan los botones
-      if( activaBotones == false ){
-        estadoBotones(true);
-      }else{
-        //activamos los activaBotones
-        estadoBotones(false);
-      }
-      console.log('activa botones es: '+activaBotones);
-
-    })
+        });
+        // si activaBotones es false se desactivan los botones
+        if( activaBotones == false ){
+          estadoBotones(true);
+        }else{
+          //activamos los activaBotones
+          estadoBotones(false);
+        }    
+    });
 
     //boton de Eliminar
     $('#eliminarModal').click(function(){
